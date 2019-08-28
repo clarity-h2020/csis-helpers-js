1997d65a747e98cbcd028310c65300c97792e03b
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-08-28 17:12:58 +0200

    #6 FUBAR

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import * as CSISRemoteHelpers from './lib/CSISRemoteHelpers.js';
 import * as EMIKATHelpers from './lib/EMIKATHelpers.js';
 import * as CSISHelpers from './lib/CSISHelpers.js';
 
-import CSISRemoteHelpersTests from './__tests__/CSISRemoteHelpers.test.js';
+//import CSISRemoteHelpersTests from './__tests__/CSISRemoteHelpers.test.js';
 
 import log from 'loglevel';
 // SEEMS TO BE REALLY NECESSARY TO GET DEBUG MESSAGES ON CONSOLE.
 log.enableAll();
 
-export {CSISHelpers, CSISRemoteHelpers, EMIKATHelpers, CSISRemoteHelpersTests}
+//export {CSISHelpers, CSISRemoteHelpers, EMIKATHelpers, CSISRemoteHelpersTests}
+export {CSISHelpers, CSISRemoteHelpers, EMIKATHelpers}
