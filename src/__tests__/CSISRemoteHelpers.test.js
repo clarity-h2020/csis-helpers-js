a2b5049a1cce7a9bb67c28c9231094e3554fe1ae
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-08-28 14:19:49 +0200

    #6 use ENV vars instead of dynamic imports

--- src/__tests__/CSISRemoteHelpers.test.js
+++ src/__tests__/CSISRemoteHelpers.test.js
@@ -4,58 +4,31 @@ import log from 'loglevel';
 import * as CSISRemoteHelpers from './../lib/CSISRemoteHelpers.js';
 import * as CSISHelpers from './../lib/CSISHelpers.js';
 
-import {CSISRemoteHelpers as _CSISRemoteHelpers, CSISHelpers as _CSISHelpers} from './../../dist/index.js';
+import { CSISRemoteHelpers as _CSISRemoteHelpers, CSISHelpers as _CSISHelpers } from './../../dist/index.js';
 
 import apiResponseStudy from './../__fixtures__/study.json';
 
-/**
- * @type {Object[]}
- */
-var headers = undefined;
-try {
-    import('./../__fixtures__/csisHeaders.js').then(
-        (module) => { headers = module.default; log.info('headers.js fixture found, executing remote API tests'); },
-        (error) => { log.info('csisHeaders.js is not present, skipping remote API tests'); }
-    );
-} catch (error) {
-    //ignore
-}
-
-let debugLogInterceptor;
+log.enableAll();
 
 /**
  * Set auth headers for live API test
  */
 beforeAll(async (done) => {
-
-    if (!headers) {
-        try {
-            const module = await import('./../__fixtures__/csisHeaders.js');
-            headers = module.default;
-            log.info('headers.js fixture found, executing remote API tests');
-        } catch (error) {
-            log.info('csisHeaders.js is not present, skipping remote API tests');
-        }
-    }
-
     //axios.defaults.withCredentials = true;
-    if (headers && Array.isArray(headers)) {
+    if (process.env.COOKIE && process.env.ORIGIN) {
         log.info('headers.js fixture found, executing remote API tests');
-        headers.forEach((header) => {
-            // this will fail when a new instance of axios has been created in CSISRemoteHelpers
-            // because the instance is created with the *previous* defaults! :o
-            //axios.defaults.headers.common[header[0]] = header[1];
+        // this will fail when a new instance of axios has been created in CSISRemoteHelpers
+        // because the instance is created with the *previous* defaults! :o
+        //axios.defaults.headers.common[header[0]] = header[1];
 
-            // therefore we change the instance in CSISRemoteHelpers :o
-            CSISRemoteHelpers.csisClient.defaults.headers.common[header[0]] = header[1];
-        });
+        // therefore we change the instance in CSISRemoteHelpers :o
+        CSISRemoteHelpers.csisClient.defaults.headers.common['Cookie'] = process.env.COOKIE;
+        CSISRemoteHelpers.csisClient.defaults.headers.common['Origin'] = process.env.Origin;
+        _CSISRemoteHelpers.csisClient.defaults.headers.common['Cookie'] = process.env.COOKIE;
+        _CSISRemoteHelpers.csisClient.defaults.headers.common['Origin'] = process.env.Origin;
     } else {
-        log.warn('no headers.js fixture found, skipping remote API tests');
+        log.warn('no headers ENV VAR (.env.test.local) found, skipping remote API tests');
     }
-    debugLogInterceptor = CSISRemoteHelpers.csisClient.interceptors.request.use((request) => {
-        //log.debug(JSON.stringify(request));
-        return request;
-    });
     done();
 });
 
@@ -69,41 +42,48 @@ beforeEach(async (done) => {
 afterAll(() => {
     //delete axios.defaults.withCredentials;
     delete CSISRemoteHelpers.csisClient.defaults.headers.common[axios.defaults.xsrfHeaderName];
-    if (headers) {
-        delete CSISRemoteHelpers.csisClient.defaults.headers.common[headers[0]];
+    if (process.env.COOKIE && process.env.ORIGIN) {
+        delete CSISRemoteHelpers.csisClient.defaults.headers.common['Cookie'];
+        delete CSISRemoteHelpers.csisClient.defaults.headers.common['Origin'];
+        delete _CSISRemoteHelpers.csisClient.defaults.headers.common['Cookie'];
+        delete _CSISRemoteHelpers.csisClient.defaults.headers.common['Origin'];
     }
-    CSISRemoteHelpers.csisClient.interceptors.request.eject(debugLogInterceptor);
 });
 
-test('get and compare X-CSRF Token', async (done) => {
-    expect.assertions(1);
-    const token1 = await CSISRemoteHelpers.getXCsrfToken();
-    const token2 = await CSISRemoteHelpers.getXCsrfToken();
-    // if not logged in by session cookie, token will be different for each request
-    if (!headers) {
-        expect(token1).not.toEqual(token2);
-    } else {
-        expect(token1).toEqual(token2);
-    }
 
-    done();
+describe('Remote API tests without authentication', () => {
+    test('get and compare X-CSRF Token', async (done) => {
+        expect.assertions(1);
+        const token1 = await CSISRemoteHelpers.getXCsrfToken();
+        const token2 = await CSISRemoteHelpers.getXCsrfToken();
+        // if not logged in by session cookie, token will be different for each request
+        if (!process.env.COOKIE || !process.env.ORIGIN) {
+            expect(token1).not.toEqual(token2);
+        } else {
+            expect(token1).toEqual(token2);
+        }
+        done();
+    });
 });
 
 describe('Remote API tests with authentication', () => {
 
-    setTimeout(function(){
-        //do what you need here
-    }, 2000);
-
-    // skip tests if local headers file containing session cookie is not available
-    if (!headers) {
+    if (!process.env.COOKIE || !process.env.ORIGIN) {
         it.only('no headers.js fixture found, skipping remote API tests', () => {
             log.warn('no headers.js fixture found, skipping remote API tests');
         });
     }
 
-    it('test get EMIKAT Credentials', async (done) => {
+    it('[DEV] test get EMIKAT Credentials', async (done) => {
         const emikatCredentials = await CSISRemoteHelpers.getEmikatCredentialsFromCsis();
+        expect.assertions(2);
+        expect(emikatCredentials).toBeDefined();
+        expect(emikatCredentials).not.toBeNull();
+        done();
+    });
+
+    it('[PROD] test get EMIKAT Credentials', async (done) => {
+        const emikatCredentials = await _CSISRemoteHelpers.getEmikatCredentialsFromCsis();
         expect.assertions(2);
         expect(emikatCredentials).toBeDefined();
         expect(emikatCredentials).not.toBeNull();
@@ -136,17 +116,17 @@ describe('Remote API tests with authentication', () => {
 
     it('[DEV] test get datapackage resources for eu-gl:hazard-characterization', async (done) => {
         const resourcesApiResponse = await CSISRemoteHelpers.getDatapackageResourcesFromCsis(undefined, "a8ff7930-4a9f-4289-8246-3383ba13c30f");
-        
+
         expect.assertions(6);
-        
+
         expect(resourcesApiResponse).toBeDefined();
         expect(resourcesApiResponse).not.toBeNull();
         expect(resourcesApiResponse.data).not.toBeNull();
         expect(resourcesApiResponse.included).not.toBeNull();
-        
+
         const filteredResources = CSISHelpers.filterResourcesByEuglId(
-            resourcesApiResponse.data, 
-            resourcesApiResponse.included,  
+            resourcesApiResponse.data,
+            resourcesApiResponse.included,
             'eu-gl:hazard-characterization');
 
         log.info(`filteredResources: ${filteredResources.length}`);
