7894753c07890c9e2cd991715312925fc085897c
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-09-05 13:14:08 +0200

    #6 add emikat parameters test

--- src/__tests__/EMIKATHelpers.test.js
+++ src/__tests__/EMIKATHelpers.test.js
@@ -11,7 +11,7 @@
 import axios from 'axios';
 
 import * as EMIKATHelpers from './../lib/EMIKATHelpers.js';
-import {EMIKATHelpers as _EMIKATHelpers} from './../../dist/index.js';
+import { EMIKATHelpers as _EMIKATHelpers } from './../../dist/index.js';
 
 
 // import {create, router, defaults, rewriter} from 'json-server';
@@ -30,41 +30,41 @@ beforeAll(() => {
   server = app.listen(31337, () => console.log('Example app listening on port 3000!'))
 });
 
-it ('tests JSON API', async (done) => {
+it('tests JSON API', async (done) => {
   expect.assertions(1);
   const response = await axios.get('http://localhost:31337/EmiKatTst/api/scenarios/2846/feature/tab.CLY_EL_POPULATION_INTERPOLATED.2016/table/data');
   expect(response.data).toEqual(populationExposure);
   done();
 });
 
-test ('[DEV] ReactTable columns defintions automatically generated from EMIKAT tabular data', ()=>{
+test('[DEV] ReactTable columns defintions automatically generated from EMIKAT tabular data', () => {
 
   const columns = EMIKATHelpers.generateColumns(populationExposure.columnnames);
   expect(columns).toBeInstanceOf(Array);
   expect(columns.length).toEqual(populationExposure.columnnames.length);
-  columns.forEach((columDefinition, index)=>{
+  columns.forEach((columDefinition, index) => {
     expect(columDefinition.Header).toEqual(populationExposure.columnnames[index]);
   });
 });
 
-test ('[RELEASE] ReactTable columns defintions automatically generated from EMIKAT tabular data', ()=>{
+test('[RELEASE] ReactTable columns defintions automatically generated from EMIKAT tabular data', () => {
 
   const columns = _EMIKATHelpers.generateColumns(populationExposure.columnnames);
   expect(columns).toBeInstanceOf(Array);
   expect(columns.length).toEqual(populationExposure.columnnames.length);
-  columns.forEach((columDefinition, index)=>{
+  columns.forEach((columDefinition, index) => {
     expect(columDefinition.Header).toEqual(populationExposure.columnnames[index]);
   });
 });
 
-test ('[RELEASE] URL without EmikatId', ()=>{
+test('[RELEASE] URL without EmikatId', () => {
 
   const url = 'https://clarity.meteogrid.com/geoserver/wms?service=WMS&version=1.1.0&request=GetMap&layers=europe:HI_Tx75p-consecutive-max_historical_19710101-20001231_ensstd&bbox=2145500,982500,6606000,57065000&width=725&height=768&srs=EPSG:3035&format=image/png';
   const transformedUrl = _EMIKATHelpers.addEmikatId(url, 'NO_ID')
   expect(url).toEqual(transformedUrl);
 });
 
-test ('[RELEASE] URL with EmikatId', ()=>{
+test('[RELEASE] URL with EmikatId', () => {
   /**
    * @type {String}
    */
@@ -77,9 +77,74 @@ test ('[RELEASE] URL with EmikatId', ()=>{
   expect(transformedUrl.includes('31337'));
 });
 
+test('[DEV] URL with EMIKAT PARAMETERS', () => {
+  /**
+   * @type {String}
+   */
+  const urlTemplate = `https://service.emikat.at/geoserver/clarity/wms?service=WMS&version=1.1.0&request=GetMap&layers=clarity%3Aview.2974&bbox=4672000.0%2C1979500.0%2C4687500.0%2C1988000.0&width=768&height=421&srs=EPSG%3A3035&format=image%2Fgif&CQL_FILTER=PROJECT%3D%27${EMIKATHelpers.EMIKAT_PROJECT_VARIANT}%27%20and%20PERIOD=%27${EMIKATHelpers.EMIKAT_PERIOD}%27%20AND%20RCP=%27${EMIKATHelpers.EMIKAT_RCP}%27%20AND%20FREQUENCE=%27${EMIKATHelpers.EMIKAT_FREQUENCY}%27%20AND%20SZ_ID=${EMIKATHelpers.EMIKAT_STUDY_ID}&styles=T_A`;
+
+  /**
+   * @type {String}
+   */
+  const url = 'https://service.emikat.at/geoserver/clarity/wms?service=WMS&version=1.1.0&request=GetMap&layers=clarity%3Aview.2974&bbox=4672000.0%2C1979500.0%2C4687500.0%2C1988000.0&width=768&height=421&srs=EPSG%3A3035&format=image%2Fgif&CQL_FILTER=PROJECT%3D%27BASELINE%27%20and%20PERIOD=%27Baseline%27%20AND%20RCP=%27Baseline%27%20AND%20FREQUENCE=%27Rare%27%20AND%20SZ_ID=2846&styles=T_A';
+
+  /**
+   * @type{Map<String, any>}
+   */
+  const parametersMap = new Map([
+    [EMIKATHelpers.EMIKAT_STUDY_ID, 2846],
+    [EMIKATHelpers.EMIKAT_PROJECT_VARIANT, 'BASELINE'],
+    [EMIKATHelpers.EMIKAT_PERIOD, 'Baseline'],
+    [EMIKATHelpers.EMIKAT_RCP, 'Baseline'],
+    [EMIKATHelpers.EMIKAT_FREQUENCY, 'Rare']
+  ]);
+
+  /**
+     * @type {String}
+     */
+  const transformedUrl = EMIKATHelpers.addEmikatParameters(urlTemplate, parametersMap);
+  expect(urlTemplate).not.toEqual(transformedUrl);
+  expect(transformedUrl.includes('2846'));
+  expect(transformedUrl.includes('BASELINE'));
+  expect(transformedUrl.includes('Baseline'));
+  expect(transformedUrl.includes('Rare'));
+  expect(url).toEqual(transformedUrl);
+});
+
+test('[PROD] URL with EMIKAT PARAMETERS', () => {
+  /**
+   * @type {String}
+   */
+  const urlTemplate = `https://service.emikat.at/geoserver/clarity/wms?service=WMS&version=1.1.0&request=GetMap&layers=clarity%3Aview.2974&bbox=4672000.0%2C1979500.0%2C4687500.0%2C1988000.0&width=768&height=421&srs=EPSG%3A3035&format=image%2Fgif&CQL_FILTER=PROJECT%3D%27${_EMIKATHelpers.EMIKAT_PROJECT_VARIANT}%27%20and%20PERIOD=%27${_EMIKATHelpers.EMIKAT_PERIOD}%27%20AND%20RCP=%27${_EMIKATHelpers.EMIKAT_RCP}%27%20AND%20FREQUENCE=%27${_EMIKATHelpers.EMIKAT_FREQUENCY}%27%20AND%20SZ_ID=${_EMIKATHelpers.EMIKAT_STUDY_ID}&styles=T_A`;
+
+  /**
+   * @type {String}
+   */
+  const url = 'https://service.emikat.at/geoserver/clarity/wms?service=WMS&version=1.1.0&request=GetMap&layers=clarity%3Aview.2974&bbox=4672000.0%2C1979500.0%2C4687500.0%2C1988000.0&width=768&height=421&srs=EPSG%3A3035&format=image%2Fgif&CQL_FILTER=PROJECT%3D%27BASELINE%27%20and%20PERIOD=%27Baseline%27%20AND%20RCP=%27Baseline%27%20AND%20FREQUENCE=%27Rare%27%20AND%20SZ_ID=2846&styles=T_A';
+
+  /**
+   * @type{Map<String, any>}
+   */
+  const parametersMap = new Map([
+    [_EMIKATHelpers.EMIKAT_STUDY_ID, 2846],
+    [_EMIKATHelpers.EMIKAT_PROJECT_VARIANT, 'BASELINE'],
+    [_EMIKATHelpers.EMIKAT_PERIOD, 'Baseline'],
+    [_EMIKATHelpers.EMIKAT_RCP, 'Baseline'],
+    [_EMIKATHelpers.EMIKAT_FREQUENCY, 'Rare']
+  ]);
 
 
-
+  /**
+   * @type {String}
+   */
+  const transformedUrl = _EMIKATHelpers.addEmikatParameters(urlTemplate, parametersMap);
+  expect(urlTemplate).not.toEqual(transformedUrl);
+  expect(transformedUrl.includes('2846'));
+  expect(transformedUrl.includes('BASELINE'));
+  expect(transformedUrl.includes('Baseline'));
+  expect(transformedUrl.includes('Rare'));
+  expect(url).toEqual(transformedUrl);
+});
 
 afterAll(() => {
   server.close(() => {
